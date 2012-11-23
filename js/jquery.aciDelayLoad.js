59f71d01257ff442f8c1589fcc3bec6fa6cfe4fb
Dragos Ursu <u_dragos@yahoo.com>
2012-11-23 11:13:50 +0200

    updated to 1.1 (fadeIn effect fix, delayed load on scroll, added callback functions to enable custom animations)

--- js/jquery.aciDelayLoad.js
+++ js/jquery.aciDelayLoad.js
@@ -1,6 +1,6 @@
 
 /*
- * aciDelayLoad jQuery Plugin v1.0
+ * aciDelayLoad jQuery Plugin v1.1
  * http://acoderinsights.ro
  *
  * Copyright (c) 2012 Dragos Ursu
@@ -8,7 +8,7 @@
  *
  * Require jQuery Library http://jquery.com
  *
- * Date: Wed Nov 21 16:20 2012 +0200
+ * Date: Fri Nov 23 10:20 2012 +0200
  */
 
 (function($){
@@ -34,7 +34,15 @@
         duration: 200,                    // fadeIn effect duration (if any)
         easing: 'linear',                 // animation effect used with fadeIn (if any)
         dataSrc: 'data-src',              // IMG attribute name for holding the real image URL
-        dataBlank: 'data-blank'           // IMG attribute name for holding the blank image URL (created at run-time)
+        dataBlank: 'data-blank',          // IMG attribute name for holding the blank image URL (created at run-time)
+        // callback called just before we switch the 'src' attribute ('fadeIn' started, if any)
+        // function (jQuery object - the image)
+        beforeLoad: null,
+        // callback called just after image load ('fadeIn' running, if any)
+        // function (jQuery object - the image)
+        afterLoad: null,
+        resizeDelay: 50,                  // delay before image load on resize (will not be loaded if goes out of view in this time)
+        scrollDelay: 100                  // delay before image load on scroll (will not be loaded if goes out of view in this time)
     };
 
     $.fn._aciDelayLoad = function(options, data){
@@ -44,29 +52,59 @@
         var _options = $.extend({}, $.fn.aciDelayLoad.defaults, options);
 
         var _visible = function (element) {
+            // check if element is in the visible area
             var html = document.documentElement;
             var rect = element.getBoundingClientRect();
             return (rect && (rect.bottom >= 0) && (rect.right >= 0) && (rect.top <= html.clientHeight) && (rect.left <= html.clientWidth));
-        }
+        };
+
+        var _load = function(image) {
+            // load the image
+            if (_options.fadeIn) {
+                image.fadeTo(0, _options.fadeIn);
+            }
+            if (_options.beforeLoad){
+                _options.beforeLoad(image);
+            }
+            image.attr(_options.dataBlank, image.attr('src')).bind('load' + $.aciDelayLoad.nameSpace, function(event){
+                var $this = $(event.target);
+                $this.unbind('load' + $.aciDelayLoad.nameSpace);
+                $this.fadeTo(_options.duration, 1, _options.easing);
+                if (_options.afterLoad){
+                    _options.afterLoad($this);
+                }
+            }).attr('src', image.attr(_options.dataSrc)).attr(_options.dataSrc, null);
+        };
+
+        var _delayLoad = function(image) {
+            if (image.attr(_options.dataBlank)){
+                // skip this image as may be in the load
+                return;
+            }
+            if (_visible(image.get(0))){
+                _load(image);
+            }
+        };
 
         var _refresh = function () {
+            // refresh images, start loading if in view and not loaded yet
             $this.find('img[' + _options.dataSrc + ']').each(function(){
                 var $this = $(this);
+                if ($this.attr(_options.dataBlank)){
+                    // skip this image as may be in the load
+                    return;
+                }
                 if (_visible(this)){
-                    $this.attr(_options.dataBlank, $this.attr('src'));
-                    if (_options.fadeIn) {
-                        $this.fadeTo(0, _options.fadeIn).attr('src', $this.attr(_options.dataSrc)).fadeTo(_options.duration, 1, _options.easing);
-                    } else {
-                        $this.attr('src', $this.attr(_options.dataSrc));
-                    }
-                    $this.attr(_options.dataSrc, null);
+                    window.setTimeout(function(){
+                        _delayLoad($this);
+                    }, _options.scrollDelay);
                 }
             });
-        }
+        };
 
         var _timeout = null;
 
-        var _enable = function(init){
+        var _enable = function(){
             _disable();
             // load images on scroll/resize
             $this.bind('scroll' + $.aciDelayLoad.nameSpace, _refresh);
@@ -75,7 +113,7 @@
                     if (_timeout) {
                         window.clearTimeout(_timeout);
                     }
-                    _timeout = window.setTimeout(_refresh, 50);
+                    _timeout = window.setTimeout(_refresh, _options.resizeDelay);
                 });
             }
         };
@@ -164,8 +202,8 @@
             // load images on update
             $this.bind('update' + $.aciDelayLoad.nameSpace, _refresh);
 
-            // trigger initial update
-            $this.trigger('update');
+            // initial load
+            _refresh();
 
         }; // end _customUi
 
